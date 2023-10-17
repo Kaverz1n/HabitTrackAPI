c39558279e748aae2f7bc5ae31dcb948887dc65a
Dmitry <dima.captan@yandex.ru>
2023-10-17 21:00:48 +0300

    added model User

---
+++ users/models.py
@@ -0,0 +1,12 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    '''
+    Модель пользователя сервиса
+    '''
+    telegram_nickname = models.CharField(max_length=32, unique=True, verbose_name='Telegram nickname')
+
+    USERNAME_FIELD = "telegram_nickname"
+    REQUIRED_FIELDS = []
