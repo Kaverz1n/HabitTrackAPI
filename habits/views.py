7237f80c9d9fd8e1ee2c94946682869ef3b46e39
Dmitry <dima.captan@yandex.ru>
2023-10-18 16:07:48 +0300

    added HabitViewSet

--- habits/views.py
+++ habits/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from habits.models import Habit
+from habits.serializers import HabitSerializer
+from habits.paginators import HabitPaginator
+
+
+class HabitViewSet(viewsets.ModelViewSet):
+    '''
+    ViewSet для модели Habit
+    '''
+    queryset = Habit.objects.all()
+    serializer_class = HabitSerializer
+    pagination_class = HabitPaginator
