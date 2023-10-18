8e71358b87f6333b5266cb76f501353c1c5a263c
Dmitry <dima.captan@yandex.ru>
2023-10-18 16:08:16 +0300

    added HabitPaginator

---
+++ habits/paginators.py
@@ -0,0 +1,9 @@
+from rest_framework.pagination import PageNumberPagination
+
+
+class HabitPaginator(PageNumberPagination):
+    '''
+    Пагинатор для модели Habit
+    '''
+    page_size = 5
+    page_size_query_param = 'per_page'
