816e2b456d4f1799bc4e8c53487dd41e76a314fd
Dmitry <dima.captan@yandex.ru>
2023-11-15 20:54:38 +0300

    deleted shared_task start_bot and added exception in send_reminders

--- telegram_bot/tasks.py
+++ telegram_bot/tasks.py
@@ -7,14 +7,6 @@ from telegram_bot.models import TelegramUser
 
 
 @shared_task
-def start_bot() -> None:
-    '''
-    Задача запускает бота в нон-стоп режиме
-    '''
-    BOT.polling(none_stop=True)
-
-
-@shared_task
 def send_reminders(*args) -> None:
     '''
     Задача отправляет напоминание пользователю о том,
@@ -22,25 +14,28 @@ def send_reminders(*args) -> None:
     :param args: данные о привычке
     :return:
     '''
-    username, action, place, time, executed_time, is_positive, reward = [arg for arg in args]
-    chat_id = TelegramUser.objects.get(username=username[1:])
+    try:
+        username, action, place, time, executed_time, is_positive, reward = [arg for arg in args]
+        chat_id = TelegramUser.objects.get(username=username[1:])
 
-    timee_to_execute = datetime.strptime(executed_time, "%H:%M:%S")
-    start_time = datetime.strptime("00:00:00", "%H:%M:%S")
-    seconds = round((timee_to_execute - start_time).total_seconds())
+        timee_to_execute = datetime.strptime(executed_time, "%H:%M:%S")
+        start_time = datetime.strptime("00:00:00", "%H:%M:%S")
+        seconds = round((timee_to_execute - start_time).total_seconds())
 
-    if not is_positive:
-        BOT.send_message(
-            chat_id=chat_id,
-            text=f'"Я буду {action} в {time[:5]} в {place}"\n\n'
-                 f'Такое обещание вы себе дали! Время выполнять привычку!\n'
-                 f'Время на выполнение: {seconds} секунд\n\n'
-                 f'Награда: {reward}'
-        )
-    else:
-        BOT.send_message(
-            chat_id=chat_id,
-            text=f'"Я буду {action} в {time[:5]} в {place}"\n\n'
-                 f'Приятно слышать! Время приятной привычки!\n'
-                 f'Время на выполнение: {seconds} секунд\n\n'
-        )
+        if not is_positive:
+            BOT.send_message(
+                chat_id=chat_id,
+                text=f'"Я буду {action} в {time[:5]} в {place}"\n\n'
+                     f'Такое обещание вы себе дали! Время выполнять привычку!\n'
+                     f'Время на выполнение: {seconds} секунд\n\n'
+                     f'Награда: {reward}'
+            )
+        else:
+            BOT.send_message(
+                chat_id=chat_id,
+                text=f'"Я буду {action} в {time[:5]} в {place}"\n\n'
+                     f'Приятно слышать! Время приятной привычки!\n'
+                     f'Время на выполнение: {seconds} секунд\n\n'
+            )
+    except TelegramUser.DoesNotExist:
+        print('Пользователь не подписался на рассылку через телеграм-бота!')
